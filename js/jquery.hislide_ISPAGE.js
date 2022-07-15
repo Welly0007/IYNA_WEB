59da97880606713266c15e19f2202858f1347af5
welly0007 <1020066@stemegypt.edu.eg>
2022-07-15 02:41:16 +0200

    asd

--- js/jquery.hislide_ISPAGE.js
+++ js/jquery.hislide_ISPAGE.js
@@ -1,4 +1,4 @@
-let query = window.matchMedia("(max-width:1366px)");
+let query = window.matchMedia("(max-width:1110px)");
 if(query.matches){
     (function($) {
         var slide = function(ele,options) {
@@ -10,9 +10,9 @@ if(query.matches){
             };
             $.extend(true, setting, options);
             var states = [
-                { $zIndex: 15, width: 250, height: 332, top: 0, left: 0, $opacity: 1 },
-                { $zIndex: 14, width: 203, height: 267, top: 32, left: 125, $opacity: 0.9 },
-                { $zIndex: 13, width: 145, height: 193, top: 64, left: 260, $opacity: 0.9 }
+                { $zIndex: 15, width: 150, height: 199, top: 0, left: 0, $opacity: 1 },
+                { $zIndex: 14, width: 130, height: 170, top: 15, left: 110, $opacity: 0.9 },
+                { $zIndex: 13, width: 0, height: 0, top: 100, left: 200, $opacity: 0.9 }
             ];
     
             var $lis = $ele.find('li');
@@ -69,9 +69,9 @@ if(query.matches){
             };
             $.extend(true, setting, options);
             var states = [
-                { $zIndex: 15, width: 364, height: 483, top: 0, left: 0, $opacity: 1 },
-                { $zIndex: 14, width: 303, height: 399, top: 40, left: 182, $opacity: 0.9 },
-                { $zIndex: 13, width: 245, height: 326, top: 80, left: 333, $opacity: 0.9 }
+                { $zIndex: 15, width: 250, height: 332, top: 0, left: 0, $opacity: 1 },
+                { $zIndex: 14, width: 203, height: 267, top: 32, left: 125, $opacity: 0.9 },
+                { $zIndex: 13, width: 145, height: 193, top: 64, left: 260, $opacity: 0.9 }
             ];
     
             var $lis = $ele.find('li');
